1e4923ed2e9fc9a9d489ee6c5ba3242718031adb
Andrew Yip <andrew.yip22@gmail.com>
2021-03-07 22:20:37 -1000

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { CoursesService } from './courses.service';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.component';
+import { CourseComponent } from './course/course.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CourseComponent,
+    CoursesComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [CoursesService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
